bf838d45e96b676228b199cfe1b89e1d2d5c0611
karpust <956277@mail.ru>
2025-07-11 23:25:42 +0000

    some project's settings for reverse proxy

--- authapp/views.py
+++ authapp/views.py
@@ -40,7 +40,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.isAuthenticated]  # AllowAny]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
